61e944d300bb91aa5b5fce5de83f88d7307f2fab
lawrencetheabhorrence <luluemilianolawrence@gmail.com>
2021-07-27 17:43:37 +0800

    cleaning up and added choice generation

--- src/QuestionParser.hs
+++ src/QuestionParser.hs
@@ -1,6 +1,7 @@
 module QuestionParser where
 import Data.List
 import Data.String
+import Control.Monad
 import qualified Data.Set as Set
 import qualified System.Random as Rand
 
@@ -25,19 +26,17 @@ getLabels = filter (startswith '%') . nub . words
 intRange :: (Integer, Integer)
 intRange = (30, 300)
 
--- generate the values for the formatters
-generateValues :: (Rand.RandomGen g) => [String] -> g -> [(String, String)]
-generateValues [] _ = []
-generateValues (('%':cs):xs) g = (cs, genVal cs):(generateValues xs g)
+genValue :: (Rand.RandomGen g) => g -> String -> (String, String)
+genValue g ('%':c:cs) = ((c:cs), genVal c)
   where 
-    genVal (c:cs) =
+    genVal c =
       case c of
         'n' -> generateName g
         't' -> generateTime g
         'i' -> generateInt g
         'd' -> generateDecimal g
         'p' -> generatePercentage g
-generateValues (_:xs) g = generateValues xs g
+genValue _ _ = ("", "")
 
 generateName :: (Rand.RandomGen g) => g -> String
 generateName _ = "bob"
@@ -49,7 +48,7 @@ generateInt :: (Rand.RandomGen g) => g -> String
 generateInt seed = show . fst $ Rand.uniformR intRange seed
 
 generateDecimal :: (Rand.RandomGen g) => g -> String
-generateDecimal seed = show . fst $ Rand.uniformR (1 :: Double, 30 :: Double) seed
+generateDecimal seed = take 4 $ show . fst $ Rand.uniformR (1 :: Double, 30 :: Double) seed
 
 generatePercentage :: (Rand.RandomGen g) => g -> String
 generatePercentage seed = (show . fst $ Rand.uniformR (1 :: Integer, 99 :: Integer) seed) ++ "%"
@@ -58,23 +57,27 @@ generatePercentage seed = (show . fst $ Rand.uniformR (1 :: Integer, 99 :: Integ
 -- (which is analogous to a hashmap in Java, or a dictionary in Python)
 -- we can now substitute it into the template
 subValues :: [(String, String)] -> String -> String
-subValues dict template = buildResult (words template) ""
+subValues dict template = concat . reverse $ buildResult (words template) []
   where
-    buildResult [] result = result
-    buildResult (('%':cs):ws) result = buildResult ws (result ++ " " ++ maybe "" id (lookup cs dict))
-    buildResult (w:ws) result = buildResult ws (result ++ " " ++ w)
-
-questionFromTemplate :: (Rand.RandomGen g) => String -> g -> String
-questionFromTemplate s g = subValues (generateValues (getLabels s) g) s
+    buildResult [] result = intersperse " " result
+    buildResult (('%':cs):ws) result = buildResult ws (maybe "" id (lookup cs dict): result)
+    buildResult (w:ws) result = buildResult ws (w : result)
 
 -- handling choice generation
-generateChoices :: (Show a, Rand.RandomGen g) => a -> g -> (g -> String) -> String
-generateChoices a seed generator = unwords $ zipWith (++) (map (++ ". ") ["A", "B", "C", "D", "E"]) ((show a) : (replicate 4 (generator seed)))
+generateChoices :: (Show a) => a -> [String] -> [String]
+generateChoices a choices = zipWith (++) (map (++ ". ") ["A", "B", "C", "D", "E"]) (show a : choices)
 
 -- now this is where we handle I/O and random generation
 -- both of which we cannot do with pure functions
 main = do
-  gen <- Rand.randomIO
-  let seed = Rand.mkStdGen gen
   let q = "%i1 %i2 %d %t3 Camilla's favorite numbers"
-  putStrLn (questionFromTemplate q seed)
+  let formatters = getLabels q
+  gens <- replicateM (length formatters) Rand.randomIO
+  let seeds = map (Rand.mkStdGen) gens
+  let valParams = zip seeds formatters
+  let values = [uncurry genValue p | p <- valParams]
+  putStrLn (subValues values q)
+  gens <- replicateM 5 Rand.randomIO
+  let choiceSeeds = map (Rand.mkStdGen) gens
+  let choices = [generateInt seed | seed <- choiceSeeds]
+  putStrLn (show $ generateChoices 1 choices)
